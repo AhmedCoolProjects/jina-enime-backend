9a4b7faa8f1818f1dec3f6de74f8c083c98d91ec
Ahmed Bargady <bargadyahmed2000@gmail.com>
2022-05-26 20:44:16 +0100

    no routers

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import studentRouter from "./StudentRouters.js";
-import workerRouter from "./WorkerRouters.js";
+// import studentRouter from "./StudentRouters.js";
+// import workerRouter from "./WorkerRouters.js";
 // import complainRouter from "./ComplainRouters.js";
 
 const router = Router();
@@ -10,8 +10,8 @@ router.get("/", (req, res) => {
   res.send("Welcome to the JINA ENIME API");
 });
 
-router.use("/student", studentRouter);
-router.use("/worker", workerRouter);
+// router.use("/student", studentRouter);
+// router.use("/worker", workerRouter);
 // router.use("/complaint", complainRouter);
 
 export default router;
