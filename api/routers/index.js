730d97383b20cb769667ed3a17920ecaa755f186
Ahmed Bargady <bargadyahmed2000@gmail.com>
2022-06-01 22:00:15 +0100

    let's see the __direname in the server

--- api/routers/index.js
+++ api/routers/index.js
@@ -41,11 +41,15 @@ complainRouter.post("/photos", upload.single("photo"), (req, res) => {
   res.send(req.file.filename);
 });
 complainRouter.get("/photo/:filename", (req, res) => {
-  res.sendFile(`uploads/${req.params.filename}`, { root: __dirname }, (err) => {
-    if (err) {
-      res.status(404).send("File not found");
+  res.sendFile(
+    `/uploads/${req.params.filename}`,
+    { root: __dirname },
+    (err) => {
+      if (err) {
+        res.status(404).send(__dirname);
+      }
     }
-  });
+  );
 });
 
 // Student
